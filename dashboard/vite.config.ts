8f17c3889b355d74d4b1227de3d982cb607228c0
Yassin Siouda <yassin@siouda.com>
2025-04-25 18:00:59 +0200

    fix(front): wip: allow hosts in vite conf

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	server: {
+		host: true, // Listen on all addresses
+		allowedHosts: ['*', 'localhost'],
+	},
 });
